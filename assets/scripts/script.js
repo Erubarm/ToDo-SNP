ecea8e59d7834fbecbe8de4db315ed36d7d023af
erubarn <kirill.komigachev@yandex.ru>
2024-01-11 12:08:36 +0300

    add li

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,20 @@
 const main = document.querySelector('main');
+const input = document.querySelector('input')
+const ul = document.querySelector('ul')
 
+console.log(ul)
+
+console.log(input)
+
+document.addEventListener('keyup', event => {
+    if (event.code === 'Enter') {
+        const li = document.createElement('li');
+        li.innerHTML += `
+        <div class="li-inner">
+            <input type="checkbox">
+            <p>${input.value}</p>
+        </div>
+        `
+        ul.appendChild(li);
+    }
+})
