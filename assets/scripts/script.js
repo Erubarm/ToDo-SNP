207777b675fa86f81937394c12c6361af1647b99
erubarn <kirill.komigachev@yandex.ru>
2024-03-02 14:04:11 +0300

    Commit

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,20 +1,27 @@
 const main = document.querySelector('main');
 const input = document.querySelector('input')
 const ul = document.querySelector('ul')
-
+const taskCross = document.getElementById('cross');
 console.log(ul)
 
 console.log(input)
 
-document.addEventListener('keyup', event => {
+input.addEventListener('keyup', event => {
     if (event.code === 'Enter') {
         const li = document.createElement('li');
         li.innerHTML += `
         <div class="li-inner">
-            <input type="checkbox">
-            <p>${input.value}</p>
+            <div class="task__input-text">
+                <input type="checkbox">
+                <p>${input.value}</p>
+            </div>
+            <button id="cross" class="button__cross"><img src="/assets/icons/cross.svg" alt="cross"></button>
         </div>
         `
         ul.appendChild(li);
     }
 })
+
+
+
+
