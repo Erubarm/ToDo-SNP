1c69f0a0a4c53c85fe59638e963bdbb545c14569
erubarm <kirill.komigachev@yandex.ru>
2024-03-03 03:43:46 +0300

    1.Добавлено удаление задачи 2. добавлены кнопки сортировки: все, активные, выполненные 3. добавлена кнопка удаления выполненных задач

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,27 +1,76 @@
-const main = document.querySelector('main');
-const input = document.querySelector('input')
-const ul = document.querySelector('ul')
-const taskCross = document.getElementById('cross');
-console.log(ul)
+const main = document.querySelector("main");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+let tasks;
+const buttonShowAll = document.getElementById("tasks-show__all");
+const buttonShowActive = document.getElementById("tasks-show__active");
+const buttonShowComplete = document.getElementById("tasks-show__complete");
+const taskCount = document.getElementById("task-count");
+const buttonRemoveCompleted = document.getElementById("remove-completed");
 
-console.log(input)
-
-input.addEventListener('keyup', event => {
-    if (event.code === 'Enter') {
-        const li = document.createElement('li');
-        li.innerHTML += `
-        <div class="li-inner">
+// console.log(taskCross);
+input.addEventListener("keyup", (event) => {
+  if (event.code === "Enter" && input.value !== "") {
+    let li = document.createElement("li");
+    li.className = "li-inner";
+    li.innerHTML = `
+        
             <div class="task__input-text">
                 <input type="checkbox">
-                <p>${input.value}</p>
+                <p onclick="changeTask(this)">${input.value}</p>
             </div>
-            <button id="cross" class="button__cross"><img src="/assets/icons/cross.svg" alt="cross"></button>
-        </div>
-        `
-        ul.appendChild(li);
+            <button class="button__cross" onclick='deleteTask(this)'><img src="assets/icons/cross.svg" alt="cross"></button>
+        
+        `;
+    input.value = "";
+    ul.appendChild(li);
+
+    tasks = ul.querySelectorAll("li");
+    console.log(tasks);
+    
+  }
+});
+
+function deleteTask(cross) {
+  cross.closest(".li-inner").remove();
+}
+
+function showAll() {
+  tasks.forEach((item) => {
+    if (item.classList.contains("hide") == true) {
+      item.classList.remove("hide");
     }
-})
+  });
+}
+
+function showActive() {
+  tasks.forEach((item) => {
+    if (item.querySelector("input").checked == true) {
+      item.classList.add("hide");
+    } else {
+      item.classList.remove("hide");
+    }
+  });
+}
+
+function showComplete() {
+  tasks.forEach((item) => {
+    if (item.querySelector("input").checked == false) {
+      item.classList.add("hide");
+    } else {
+      item.classList.remove("hide");
+    }
+  });
+}
 
 
+
+function removeCompleted() {
+  tasks.forEach((item) => {
+    if (item.querySelector("input").checked == true) {
+      item.remove();
+    }
+  });
+}
 
 
